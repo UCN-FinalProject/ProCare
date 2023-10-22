0ef65e281c65d24261d0b52d19fa2b80f37b4156
Tomáš Sabol <tomassabol2@gmail.com>
2023-10-22 23:59:22 +0200

    added swagger-like trpc panel for api testing

---
+++ src/app/api/trpc/panel/route.ts
@@ -0,0 +1,20 @@
+import { renderTrpcPanel } from "trpc-panel";
+import { appRouter } from "~/server/api/root";
+import { env } from "~/env.mjs";
+
+const panelHandler = () => {
+  if (env.NODE_ENV === "production") {
+    return new Response("Not found", { status: 404 });
+  }
+  return new Response(
+    renderTrpcPanel(appRouter, {
+      url: "http://localhost:3000/api/trpc",
+      transformer: "superjson",
+    }),
+    {
+      headers: { "Content-Type": "text/html" },
+    },
+  );
+};
+
+export { panelHandler as GET, panelHandler as POST };
