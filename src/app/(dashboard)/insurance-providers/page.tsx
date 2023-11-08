5bc47ac706b210803c90daf934f150479f8a3b92
Tomáš Sabol <tomassabol2@gmail.com>
2023-11-08 21:30:05 +0100

    logout styling

--- src/app/(dashboard)/insurance-providers/page.tsx
+++ src/app/(dashboard)/insurance-providers/page.tsx
@@ -18,7 +18,7 @@ export default async function page() {
       <div className="flex justify-between">
         <PageHeader>Insurance providers</PageHeader>
         {/* TODO: Hide button if user is not admin */}
-        <Button>
+        <Button variant="secondary">
           <Link
             href="/insurance-providers/create"
             className="flex items-center gap-1"
--- src/components/Navigation/LogOutButton.tsx
+++ src/components/Navigation/LogOutButton.tsx
@@ -1,15 +1,41 @@
 "use client";
-
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "~/components/ui/alert-dialog";
 import { LogOut } from "lucide-react";
 import { signOut } from "next-auth/react";
 
 export default function LogoutButton() {
   return (
-    <button
-      onClick={() => signOut()}
-      className="rounded-lg p-1.5 text-stone-700 transition-all duration-150 ease-in-out hover:bg-stone-200 active:bg-stone-300 dark:text-white dark:hover:bg-stone-700 dark:active:bg-stone-800"
-    >
-      <LogOut width={18} />
-    </button>
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <button className="rounded-lg p-1.5 text-stone-700 transition-all duration-150 ease-in-out hover:bg-stone-200 active:bg-stone-300 dark:text-white dark:hover:bg-stone-700 dark:active:bg-stone-800">
+          <LogOut width={18} />
+        </button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you sure you want to log out?</AlertDialogTitle>
+          <AlertDialogDescription>
+            You are about to log out of your account. Are you sure you want to
+            proceed?
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <div onClick={() => signOut()} onKeyDown={() => signOut()}>
+            <AlertDialogAction>Log out</AlertDialogAction>
+          </div>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 }
