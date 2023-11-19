90faaa6d96697a79e81036a4628a73e99b3c9a26
kir40lmao <93580277+kir40lmao@users.noreply.github.com>
2023-11-19 22:41:54 +0100

    Patient Validation

---
+++ src/server/service/validation/PatientValidation.ts
@@ -0,0 +1,38 @@
+import { z } from "zod";
+
+export const getPatientInput = z.object({
+  name: z.string(),
+  address: z.string().optional(),
+});
+export type GetPatientInput = z.infer<typeof getPatientInput>;
+
+export const createPatientInput = z.object({
+  fullName: z.string(),
+  address: z.string(),
+  personalDoctorID: z.number(), // do we want these as optinal? maybe you link them afterwards?
+  healthInsuranceID: z.number(),
+});
+export type CreatePatientInput = z.infer<typeof createPatientInput>;
+
+export const updatePatientInput = z.object({
+  fullName: z.string(),
+  address: z.string(),
+  personalDoctorID: z.number(), // do we want these as optinal? maybe you link them afterwards?
+  healthInsuranceID: z.number(),
+});
+export type UpdatePatientInput = z.infer<typeof updatePatientInput>;
+
+export const addPatientCondition = z.object({
+  patientID: z.number(),
+  conditionID: z.number(),
+});
+export type AddPatientConditionInput = z.infer<typeof addPatientCondition>;
+
+export const addPatientProcedure = z.object({
+  patientID: z.number(),
+  procedureID: z.number(),
+  date: z.date(),
+  doctorName: z.string(),
+  doctorID: z.number(),
+});
+export type AddPatientProcedureInput = z.infer<typeof addPatientProcedure>;
