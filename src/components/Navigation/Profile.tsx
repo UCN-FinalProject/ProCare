4a8b9989c92d6c38eeab6b375daef5cac249f4dd
Tomáš Sabol <tomassabol2@gmail.com>
2023-11-22 14:01:29 +0100

    replace user image

--- src/components/Navigation/Profile.tsx
+++ src/components/Navigation/Profile.tsx
@@ -12,12 +12,11 @@ export default async function Profile() {
         href="/settings/account"
         className="flex w-full flex-1 items-center space-x-3 rounded-lg px-2 py-1.5 transition-all duration-150 ease-in-out hover:bg-stone-200 active:bg-stone-300 dark:text-white dark:hover:bg-stone-700 dark:active:bg-stone-800"
       >
-        {/* TODO: replace with doctor image and user details */}
         <Image
-          src={`https://avatar.vercel.sh/tomassabol2@gmail.com`}
-          width={40}
-          height={40}
-          alt={"Tomáš's Sabol avatar" ?? "User avatar"}
+          src="/doctor.png"
+          width={70}
+          height={70}
+          alt={`${session?.user.name ?? "User"}'s avatar`}
           className="h-6 w-6 rounded-full"
         />
         <span className="truncate text-sm font-medium">
