ed2dcc27e0eb1e9a2ab8c7f5cb682e9baf4ad4bb
Tomáš Sabol <tomassabol2@gmail.com>
2023-11-25 11:53:55 +0100

    remove procedure

--- src/server/db/schema/procedure.ts
+++
@@ -1,6 +0,0 @@
-// import { pgTable, serial, varchar } from "drizzle-orm/pg-core";
-
-// export const procedure = pgTable("procedures", {
-//   id: serial("id").primaryKey(),
-//   name: varchar("procedure_name").notNull(),
-// });
