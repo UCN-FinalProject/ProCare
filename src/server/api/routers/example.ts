701b15a0da0fa1564d69516268765a3e53f15a97
Tomáš Sabol <tomassabol2@gmail.com>
2023-12-15 21:43:59 +0100

    ancrypt and decrypt example endpoints

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { decryptText, encryptText } from "~/server/encrypt";
 
 export const exampleRouter = createTRPCRouter({
   hello: publicProcedure
@@ -8,6 +9,24 @@ export const exampleRouter = createTRPCRouter({
     .query(({ input }) => {
       return {
         greeting: `Hello ${input.text}`,
+      };
+    }),
+
+  encrypt: publicProcedure
+    .input(z.object({ text: z.string() }))
+    .query(({ input }) => {
+      return {
+        original: input.text,
+        encrypted: encryptText(input.text),
+      };
+    }),
+
+  decrypt: publicProcedure
+    .input(z.object({ text: z.string() }))
+    .query(({ input }) => {
+      return {
+        original: input.text,
+        decrypted: decryptText(input.text),
       };
     }),
 });
