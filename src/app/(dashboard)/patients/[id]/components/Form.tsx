b0b383346fd63b864ef50714ad3a539b0798a46c
Tomáš Sabol <tomassabol2@gmail.com>
2023-12-16 20:20:19 +0100

    create patient schema export

--- src/app/(dashboard)/patients/[id]/components/Form.tsx
+++ src/app/(dashboard)/patients/[id]/components/Form.tsx
@@ -44,56 +44,11 @@ import { useRouter } from "next/navigation";
 import { Calendar } from "~/components/ui/calendar";
 import { CalendarIcon } from "lucide-react";
 import { Separator } from "~/components/ui/separator";
-
-const disabilities = [
-  "limited_physical",
-  "physical",
-  "mental",
-  "none",
-] as const;
+import { patientFormSchema } from "../../PatientFormSchema";
 
 const formSchema = z.object({
   id: z.string(),
-  fullName: z.string().min(1, {
-    message: "Full name cannot be empty.",
-  }),
-  biologicalSex: z.enum(["male", "female"]),
-  dateOfBirth: z.date(),
-  ssn: z.string(),
-  recommendationDate: z.date(),
-  acceptanceDate: z.date().optional(),
-  startDate: z.date(),
-  expectedEndOfTreatment: z.date(),
-  endDate: z.date().optional(),
-  insuredID: z.string().min(1, {
-    message: "Patient's insured id is required..",
-  }),
-  email: z.string().optional(),
-  phone: z.string().optional(),
-  disability: z.enum(disabilities),
-  alergies: z.string().optional(),
-  note: z.string().optional(),
-  // address
-  address1: z.string().min(1, {
-    message: "Patient must have main address.",
-  }),
-  address2: z.string().optional(),
-  city: z.string().min(1, {
-    message: "Patient must contain city.",
-  }),
-  zip: z.string().min(1, {
-    message: "Zip code is required.",
-  }),
-  // healthcare info
-  healthInsuranceID: z.coerce.number().min(1, {
-    message: "Patient's health insurance id is required",
-  }),
-  doctorID: z.coerce.number().min(1, {
-    message: "Patient's personal doctor's id is required.",
-  }),
-  healthcareProviderID: z.coerce.number().min(1, {
-    message: "ID of the health insurance provider is required.",
-  }),
+  ...patientFormSchema.shape,
 });
 
 export default function UpdatePatientForm({
--- src/app/(dashboard)/patients/create/form.tsx
+++ src/app/(dashboard)/patients/create/form.tsx
@@ -42,55 +42,10 @@ import type {
   HealthcareProvider,
 } from "~/server/db/export";
 import { Separator } from "~/components/ui/separator";
-
-const disabilities = [
-  "limited_physical",
-  "physical",
-  "mental",
-  "none",
-] as const; //will add into Patient schema/validation later
+import { patientFormSchema } from "../PatientFormSchema";
 
 const formSchema = z.object({
-  fullName: z.string().min(1, {
-    message: "Full name cannot be empty.",
-  }),
-  biologicalSex: z.enum(["male", "female"]),
-  dateOfBirth: z.date(),
-  ssn: z.string(),
-  recommendationDate: z.date(),
-  acceptanceDate: z.date().optional(),
-  startDate: z.date(),
-  expectedEndOfTreatment: z.date(),
-  endDate: z.date().optional(),
-  insuredID: z.string().min(1, {
-    message: "Patient's insured id is required..",
-  }),
-  email: z.string().optional(),
-  phone: z.string().optional(),
-  disability: z.enum(disabilities),
-  alergies: z.string().optional(),
-  note: z.string().optional(),
-  // address
-  address1: z.string().min(1, {
-    message: "Patient must have main address.",
-  }),
-  address2: z.string().optional(),
-  city: z.string().min(1, {
-    message: "Patient must contain city.",
-  }),
-  zip: z.string().min(1, {
-    message: "Zip code is required.",
-  }),
-  // healthcare info
-  healthInsuranceID: z.coerce.number().min(1, {
-    message: "Patient's health insurance id is required",
-  }),
-  doctorID: z.coerce.number().min(1, {
-    message: "Patient's personal doctor's id is required.",
-  }),
-  healthcareProviderID: z.coerce.number().min(1, {
-    message: "ID of the health insurance provider is required.",
-  }),
+  ...patientFormSchema.shape,
 });
 
 export default function CreatePatientForm({
