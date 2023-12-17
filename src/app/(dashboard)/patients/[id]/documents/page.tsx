1dfdba22994d230f32d287e03f3ae9ad282944f2
Tomáš Sabol <tomassabol2@gmail.com>
2023-12-17 21:07:08 +0100

    fix duplication

--- src/app/(dashboard)/patients/[id]/documents/page.tsx
+++ src/app/(dashboard)/patients/[id]/documents/page.tsx
@@ -1,28 +1,11 @@
 import React from "react";
 import PageHeader from "~/components/Headers/PageHeader";
-import { getServerAuthSession } from "~/server/auth";
-import { api } from "~/trpc/server";
-import PatientHeader from "../components/PatientHeader";
 import { reportFiles } from "~/app/api/reports/route";
 import ReportCard from "./ReportCard";
 
-export default async function Page({
-  params,
-}: Readonly<{ params: { id: string } }>) {
-  const { id } = params;
-  const patient = await api.patient.getByID.query({ id });
-  const session = await getServerAuthSession();
-  const isAdmin = session?.user.role === "admin";
-
+export default function Page() {
   return (
-    <div className="flex flex-col gap-4">
-      <PatientHeader
-        id={id}
-        fullName={patient.fullName}
-        isActive={patient.isActive}
-        isAdmin={isAdmin}
-      />
-
+    <>
       <PageHeader className="text-xl">Documents</PageHeader>
 
       <div className="flex flex-wrap gap-5">
@@ -30,6 +13,6 @@ export default async function Page({
           <ReportCard key={crypto.randomUUID()} report={report} />
         ))}
       </div>
-    </div>
+    </>
   );
 }
